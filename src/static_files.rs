216d38349fde6bf365566a44a4ebd30f9b5a4927
parthasai199 <98951463+parthasai199@users.noreply.github.com>
2022-02-23 13:17:18 +0530

    Update static_files.rs

--- src/static_files.rs
+++ src/static_files.rs
@@ -1,18 +1,11 @@
 use std::io;
 use rocket::fs::NamedFile;
 use rocket::get;
-//use std::path::{Path, PathBuf};
 
 #[get("/")]
 pub async fn index() -> io::Result<NamedFile>{
 
     NamedFile::open("public/index.html").await
 }
-
-/*#[get("/<file..>", rank = 5)]
-pub async fn all(file: PathBuf) -> Option<NamedFile>{
-
-    NamedFile::open(Path::new("public/").join(file)).await.ok()
-}*/
 
 
